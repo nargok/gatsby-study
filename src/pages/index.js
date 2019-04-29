ffbf8031505bedbb519d22fb4bf1fb592fda9ea7
nargok <nargok@gmail.com>
2019-04-29 14:39:48 +0900

    display work data through graphql

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,36 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
+const IndexPage = ({ data }) => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+    <h1>Hello Great Gatsbt</h1>
+    {data.allWorksYaml.edges.map(edge => {
+      const work = edge.node
+      return <div>
+        {work.title} - {work.category} - {work.year}
+      </div>
+    })}
     <Link to="/page-2/">Go to page 2</Link>
     <Link to="/about/">Go to about page</Link>
   </Layout>
 )
+
+export const query = graphql`
+  query {
+    allWorksYaml {
+      edges {
+        node {
+          title
+          category
+          year
+        }
+      }
+    }
+  }
+`;
 
 export default IndexPage
